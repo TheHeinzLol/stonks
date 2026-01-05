49adca45fc4156c7000b89b712e099afdcae3ac9
Dmitry <mutasovdmitry@gmail.com>
2026-01-05 13:04:45 +0700

    so news apis are worthless since ratelimits are absurdly low or news are 24 hours delayed. Ok, focusing on ticker data only. Whatever.

--- airflow_learning/dags/dag_get_news.py
+++ airflow_learning/dags/dag_get_news.py
@@ -12,7 +12,8 @@ client = Minio(
     secret_key=password_minio,
     secure=False
     )
-
+date_from = pendulum.today().date().subtract(days=7).isoformat()
+date_to = pendulum.today().date().isoformat()
 bucket_name="bronze.news"
 #check if bucket exists
 tasks_alpaca.create_bucket(bucket_name, client)
@@ -23,5 +24,10 @@ news_list = list_objects(bucket_name, prefix=prefix)
 if len(news_list)>0:
     latest_ticker = sorted(news_list, reverse=False)[-1]
 #pull 30 tickers from latest for today
-    
+if ticker_list.index(latest_ticker)+30 < ticker_list:
+    #get news for ticker_list[latest_ticker_index+1:]
+    pass
+else:
+    for ticker in ticker_list[latest_ticker_index+1:latest_ticker_index+31]:
+        requestResponse = requests.get(f"https://finnhub.io/api/v1/company-news?token={credentials['finnhub']}&symbol={ticker}&from={date_from}&to=2025-11-03")
 #save to bucket
